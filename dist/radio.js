e7cbcf98f01e50d5521f660d95362478a6fae91a
Shivam Kumar Saraffa <shivam.saraffa@hbwsl.com>
2024-02-27 18:45:23 +0530

    More options added to radio

--- dist/radio.js
+++ dist/radio.js
@@ -12,24 +12,45 @@ function main () {
     // }
 
 
-    $('#div3').click(function(){        
-        if ($('#input3').is(':checked')) {     
-            $(this).css('background-color','#4590c4') 
-            $('#div4').css('background-color','#60a2ae')           
-        } else {
-            $(this).css('background-color','#60a2ae')
-        }
-    })
     $('#div4').click(function(){        
         if ($('#input4').is(':checked')) {     
             $(this).css('background-color','#4590c4')
             $('#div3').css('background-color','#60a2ae')           
-        } else {
-            $(this).css('background-color','#60a2ae');
-        }
+            $('#div5').css('background-color','#60a2ae')           
+            $('#div6').css('background-color','#60a2ae')           
+            $('#div7').css('background-color','#60a2ae')            
+        } 
+    })
+    $('#div5').click(function(){        
+        if ($('#input5').is(':checked')) {     
+            $(this).css('background-color','#4590c4')
+            $('#div3').css('background-color','#60a2ae')           
+            $('#div4').css('background-color','#60a2ae')           
+            $('#div6').css('background-color','#60a2ae')           
+            $('#div7').css('background-color','#60a2ae')           
+        } 
+    })
+    $('#div6').click(function(){        
+        if ($('#input6').is(':checked')) {     
+            $(this).css('background-color','#4590c4')
+            $('#div3').css('background-color','#60a2ae')           
+            $('#div4').css('background-color','#60a2ae')           
+            $('#div5').css('background-color','#60a2ae')           
+            $('#div7').css('background-color','#60a2ae')           
+        } 
+    })
+    $('#div7').click(function(){        
+        if ($('#input7').is(':checked')) {     
+            $(this).css('background-color','#4590c4')
+            $('#div3').css('background-color','#60a2ae')           
+            $('#div4').css('background-color','#60a2ae')           
+            $('#div5').css('background-color','#60a2ae')           
+            $('#div6').css('background-color','#60a2ae')           
+        } 
     })
 
-    $('#div3').hover(function(){
+    
+    $('#div4').hover(function(){
         $(this).css('position','relative');
         $(this).css('left','2px');
         $(this).css('top','2px');
@@ -39,7 +60,27 @@ function main () {
         $(this).css('top','0');
 
     });
-    $('#div4').hover(function(){
+    $('#div5').hover(function(){
+        $(this).css('position','relative');
+        $(this).css('left','2px');
+        $(this).css('top','2px');
+    },function(){
+        $(this).css('position','relative');
+        $(this).css('left','0');
+        $(this).css('top','0');
+
+    });
+    $('#div6').hover(function(){
+        $(this).css('position','relative');
+        $(this).css('left','2px');
+        $(this).css('top','2px');
+    },function(){
+        $(this).css('position','relative');
+        $(this).css('left','0');
+        $(this).css('top','0');
+
+    });
+    $('#div7').hover(function(){
         $(this).css('position','relative');
         $(this).css('left','2px');
         $(this).css('top','2px');
